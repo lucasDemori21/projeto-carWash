d761a030c84e534a6347cf2dfc61fedbc3185bdc
Lucas Gabriel Tamanini Demori <lucas-demori2001@hotmail.com>
2023-05-22 09:46:41 -0300

    Update carwash.sql

--- carwash.sql
+++ carwash.sql
@@ -126,15 +126,15 @@ INSERT INTO horarios_disp(id_data, data_disp, hora_disp) VALUES
 
 INSERT INTO usuarios (id_user, cpf, data_nasc, idade, username, email, pass_key, 
 cellphone, telephone, address, number_home, complement, district, city, state, zip, 
-data_reg, sts) VALUES(null,'111.111.111-11', '2004-01-01','19', 'Lucas TESTE', 
-'lucas@gmail.com', '$2y$10$vNCgLuOiTEo1m/xSea3Ke..GSdPEKcWkWJ4phTnmYz12TRAECLBz2',
+data_reg, sts) VALUES(null,'111.111.111-11', '2004-01-01','19', 'Joãozinho', 
+'cliente@gmail.com', '$2y$10$vNCgLuOiTEo1m/xSea3Ke..GSdPEKcWkWJ4phTnmYz12TRAECLBz2',
 '(99) 99999-9999', '(44) 4444-4444','Rua Claudio Lopes','1200','Casa', 'Aventureiro',
 'Joinville','SC','89225-721', '2023-05-19 15:56:05', '0');
 
 INSERT INTO funcionarios( id_func, nome_func, email, data_nasc, idade, cpf, funcao, 
 salario, senha, cep, endereco, number_home, celular, telefone, bairro, cidade, 
 estado, data_emissao, permissao, sts) VALUES (null, 'Admin', 'admin@admin.com', 
-'2001-01-01', '22', '118.972.429-48', 'Recursos Humanos', '20000.00', 
+'2001-01-01', '22', '133.234.455-90', 'Recursos Humanos', '20000.00', 
 '$2y$10$m29XYU4qb5Mog11RKP.cyeL/KtngOBdAzJ22Q45iFXGMmdBuU77QG', 
 '89225-721', 'Rua Claudio Lopes', '1281', '(47) 99635-6349', '(47) 3227-6855', 
 'Aventureiro', 'Joinville', 'SC', '2023-05-19', '1', '1');
